4367d7899121b0a4da206bbd3a7725caef874a45
thomasblomseth <thomasblomseth@hotmail.com>
2016-06-04 21:49:16 +0200

    test email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => 'https://salty-sierra-28331.herokuapp.com/' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :domain               => 'gmail.com',
+      :port                 => 587,
+      :user_name            =>  "lapshop2016@gmail.com",
+      :password             => "lapshop123",
+      :authentication       => 'plain',
+      :enable_starttls_auto => true
+  }
 end
